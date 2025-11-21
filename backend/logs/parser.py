29bc5595c6653246635026b379be43a464561933
daniel <pedrodaniel.fernandezguerrero@alum.uca.es>
2025-11-21 18:48:08 +0100

    correccion de horas de 1 y 2 digitos. Haciendo el parser mas flexible

--- backend/logs/parser.py
+++ backend/logs/parser.py
@@ -14,7 +14,11 @@ from logs.models import MysqlLogLine
 #Tiene los campos que extraeremos: date, time, thread_id, command_type, argument
 
 LOG_PATTERN = re.compile(
-    r'(?P<date>\d{6})\s+(?P<time>\d{2}:\d{2}:\d{2})\s+(?P<thread_id>\d+)\s+(?P<command_type>\w+)\s*(?P<argument>.*)'
+    r'(?P<year>\d{2})(?P<month>\d{1,2})(?P<day>\d{1,2})\s+'
+    r'(?P<time>\d{1,2}:\d{1,2}:\d{1,2})\s+'
+    r'(?P<thread_id>\d+)\s+'
+    r'(?P<command_type>\w+)\s*'
+    r'(?P<argument>.*)'
 )
 
 def parse_mysql_log(filepath):
@@ -33,17 +37,15 @@ def parse_mysql_log(filepath):
             if not match:
                 continue
 
-            # Fecha YYMMDD
-            date_raw = match.group('date')
-            year = 2000 + int(date_raw[0:2])  # 25 → 2025
-            month = int(date_raw[2:4])
-            day = int(date_raw[4:6])
-
-            #hora
-            time_str = match.group('time')
             
             #Crea datetime real con todo lo anterior
-            timestamp = datetime.strptime(f"{year}-{month}-{day} {time_str}", "%Y-%m-%d %H:%M:%S")
+            year = 2000 + int(match.group('year'))
+            month = int(match.group('month'))
+            day = int(match.group('day'))
+            hour, minute, second = map(int, match.group('time').split(':'))
+
+            timestamp = datetime(year, month, day, hour, minute, second)
+
 
 
             #Crea el registro en la base de datos.
