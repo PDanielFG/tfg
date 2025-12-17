262930616914986bd93feb7e27b122a7d03dacef
daniel <pedrodaniel.fernandezguerrero@alum.uca.es>
2025-12-18 00:15:25 +0100

    Rótulo "Graficos generales"

--- frontend/src/views/UserGraphics.vue
+++ frontend/src/views/UserGraphics.vue
@@ -17,20 +17,26 @@
         </div>
 
         <!-- Gráfico de duración de conexión del usuario -->
-        <div class="mb-8">
+        <div class="mb-12">
             <ChartUserConnectionDuration :username="selectedUser" />
         </div>
 
-        <div class="mb-8 flex justify-center flex-wrap gap-6">
+        <div class="mt-12 pt-1">
+            <h2 class="text-xl font-semibold text-gray-700 mb-1 mt-4 text-center">
+                Gráficos generales
+            </h2>
 
-            <div class="flex-1 min-w-[300px] max-w-[400px] bg-white p-4 rounded-lg shadow">
-                <ChartQueriesByType :username="selectedUser" />
-            </div>
-            <div class="flex-1 min-w-[300px] max-w-[400px] bg-white p-4 rounded-lg shadow">
-                <ChartTablesRef :username="selectedUser" />
-            </div>
-            <div class="flex-1 min-w-[300px] max-w-[400px] bg-white p-4 rounded-lg shadow">
-                <ChartColumsMostUsed :username="selectedUser" />
+            <div class="mb-8 flex justify-center flex-wrap gap-6 m5">
+
+                <div class="flex-1 min-w-[300px] max-w-[400px] bg-white p-4 rounded-lg shadow">
+                    <ChartQueriesByType :username="selectedUser" />
+                </div>
+                <div class="flex-1 min-w-[300px] max-w-[400px] bg-white p-4 rounded-lg shadow">
+                    <ChartTablesRef :username="selectedUser" />
+                </div>
+                <div class="flex-1 min-w-[300px] max-w-[400px] bg-white p-4 rounded-lg shadow">
+                    <ChartColumsMostUsed :username="selectedUser" />
+                </div>
             </div>
         </div>
     </div>
